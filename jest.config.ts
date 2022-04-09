f7ced899fe765cd47c17533f4cbb1e0ea3d8502d
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-04-09 16:24:14 -0300

    chore: add jest configs and scripts

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ export default {
   roots: ['<rootDir>/src'],
   clearMocks: true,
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/ *.ts/'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   transform: {
