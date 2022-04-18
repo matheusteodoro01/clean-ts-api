6f545e9df4d910381b6dcfe3f85d88e8052a2b74
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-04-18 08:31:54 -0300

    feat: ensure cors is enable

---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Cors Middleware', () => {
+  test('Shold enable cors', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send()
+    })
+
+    await request(app)
+      .get('/test_cors')
+      .expect('acess-control-allow-origin', '*')
+      .expect('acess-control-allow-methods', '*')
+      .expect('acess-control-allow-headers', '*')
+  })
+})
