110fd4554fe0019aaa6559e1eabe06713582ad00
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-04-18 07:36:13 -0300

    test: ensure AccountMongoRepository returns an account on sucess

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    roots: ['<rootDir>/src'],
-    clearMocks: true,
-    collectCoverage: true,
-    collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-    coverageDirectory: 'coverage',
-    coverageProvider: 'v8',
-    preset: '@shelf/jest-mongodb',
-    transform: {
-        '.+\\.ts$': 'ts-jest'
-    }
+  roots: ['<rootDir>/src'],
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  preset: '@shelf/jest-mongodb',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
 
 }
