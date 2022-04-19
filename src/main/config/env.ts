204db66c09256b539913d429a0e4cf5e49cba9c0
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-04-19 08:47:20 -0300

    feat: connect to mongo db before app listen

---
+++ src/main/config/env.ts
@@ -0,0 +1,4 @@
+export default ({
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb+srv://matheus:P1IvdDcsqmEQtLPL@cluster0.dzkxh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  port: process.env.PORT ?? '5050'
+})
