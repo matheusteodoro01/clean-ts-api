b74145462cb6b210bb9e94ddce8902f0d4f27b00
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-05-23 08:25:01 -0300

    refactor: move signup file to folder signup

--- src/main/factories/singup/signup.ts
+++ src/main/factories/singup/signup.ts
@@ -1,10 +1,10 @@
-import { DbAddAccount } from '../../data/usecases/add-acount/db-add-account'
-import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
-import { SignUpController } from '../../presentation/controllers/signup/signup'
-import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
-import { LogControllerDecorator } from '../decorators/log'
+import { DbAddAccount } from '../../../data/usecases/add-acount/db-add-account'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter'
+import { SignUpController } from '../../../presentation/controllers/signup/signup'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
+import { LogControllerDecorator } from '../../decorators/log'
 
-import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
+import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
 import { makeSignUpValidation } from './signup-validation'
 
 export const makeSignUpController = (): LogControllerDecorator => {
