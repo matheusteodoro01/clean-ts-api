b4659e0e05364fcff8888c13ecb4113f8e11bef0
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-05-26 11:16:44 -0300

    test: ensure DbAuthentication return null if LoadAccountByEmailRepository returns null

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -48,4 +48,11 @@ describe('DbAuthentication UseCase', () => {
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return null if LoadAccountByEmailRepository returns null', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockResolvedValueOnce(Promise.resolve(null))
+    const authetication = await sut.auth(makeFakeAuthentication())
+    expect(authetication).toBeNull()
+  })
 })
