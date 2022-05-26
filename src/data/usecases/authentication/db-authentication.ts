07b3177e97396d5523d00d6df594292ef675a325
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-05-26 15:24:21 -0300

    feat: ensure DbAuthenticationUseCase call UpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,12 +2,15 @@ import { LoadAccountByEmailRepository } from '../../../data/protocols/db/load-ac
 import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication'
 import { HashCompare } from '../../protocols/criptography/hash-comparer'
 import { TokenGenerator } from '../../protocols/criptography/token-generator'
+import { UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository'
 
 export class DbAuthetication implements Authentication {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hashCompare: HashCompare,
-    private readonly tokenGenerator: TokenGenerator) {}
+    private readonly tokenGenerator: TokenGenerator,
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
+  ) {}
 
   async auth (authentication: AuthenticationModel): Promise<string | null> {
     const account = await this.loadAccountByEmailRepository.load(authentication.email)
@@ -17,6 +20,7 @@ export class DbAuthetication implements Authentication {
       return await Promise.resolve(null)
     }
     const token = await this.tokenGenerator.generate(account.id)
+    await this.updateAccessTokenRepository.update(account.id, token)
     return token
   }
 }
