30ab23334403b8043b7375d8ecf4b107faf6c06d
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-05-30 08:27:16 -0300

    refactor: rename folder accont-repository to account

--- src/main/factories/singup/signup.ts
+++ src/main/factories/singup/signup.ts
@@ -1,7 +1,7 @@
 import { DbAddAccount } from '../../../data/usecases/add-acount/db-add-account'
 import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { SignUpController } from '../../../presentation/controllers/signup/signup'
-import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
 import { LogControllerDecorator } from '../../decorators/log'
 
 import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
