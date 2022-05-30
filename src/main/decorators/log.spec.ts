9af3796acd199885c8ec346e0ea543ab0416e011
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-05-30 08:33:06 -0300

    refactor: rename move log protocols to a folder

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,5 +1,5 @@
 
-import { LogErrorRepository } from '../../data/protocols/db/log-error-repository'
+import { LogErrorRepository } from '../../data/protocols/db/log/log-error-repository'
 import { AccountModel } from '../../domain/models/account'
 import { ok, serverError } from '../../presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
