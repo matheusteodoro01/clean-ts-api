f66b18ecb1037763b89b5ddd1fb66257ad223d6a
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-05-30 19:36:53 -0300

    refactor: rename signup routes with login routes

--- src/tests/signup-routes.test.ts
+++ src/tests/signup-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 import app from '../main/config/app'
 
-describe('Signup Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(global.__MONGO_URI__)
   })
@@ -15,15 +15,18 @@ describe('Signup Routes', () => {
     const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
-  test('Shold return an account on sucess', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send(({
-        name: 'Matheus',
-        email: 'matheusteodoro01@hotmail.com',
-        password: '1234',
-        passwordConfirmation: '1234'
-      }))
-      .expect(200)
+
+  describe('POST /singup', () => {
+    test('Shold return 200 on sigup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send(({
+          name: 'Matheus',
+          email: 'matheusteodoro01@hotmail.com',
+          password: '1234',
+          passwordConfirmation: '1234'
+        }))
+        .expect(200)
+    })
   })
 })
