294ff33572bfbdf9ba3dc6828a1cbb8e840ab631
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-05-30 20:08:23 -0300

    test: add login route and rename signup-routes to login-routes

--- src/tests/login-routes.test.ts
+++ src/tests/login-routes.test.ts
@@ -1,7 +1,11 @@
+import { Collection } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 import app from '../main/config/app'
 
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
 describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(global.__MONGO_URI__)
@@ -12,7 +16,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -25,6 +29,24 @@ describe('Login Routes', () => {
           email: 'matheusteodoro01@hotmail.com',
           password: '1234',
           passwordConfirmation: '1234'
+        }))
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Shold return 200 on sigup', async () => {
+      const password = await hash('1234', 12)
+      await accountCollection.insertOne({
+        name: 'Matheus',
+        email: 'matheusteodoro01@hotmail.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send(({
+          email: 'matheusteodoro01@hotmail.com',
+          password: '1234'
         }))
         .expect(200)
     })
