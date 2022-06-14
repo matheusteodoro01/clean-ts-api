a1794081ac314e330f15873bc5fa7fa68a8c5410
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-06-14 09:20:17 -0300

    feat: ensure SignupController return 403 if AddAccount returns null

--- src/main/factories/singup/signup-factory.ts
+++ src/main/factories/singup/signup-factory.ts
@@ -1,3 +1,4 @@
+import env from '../../config/env'
 import { DbAddAccount } from '../../../data/usecases/add-acount/db-add-account'
 import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { SignUpController } from '../../../presentation/controllers/signup/signup-controller'
@@ -6,13 +7,17 @@ import { LogControllerDecorator } from '../../decorators/log-controller.decorato
 
 import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
 import { makeSignUpValidation } from './signup-validation-factory'
+import { DbAuthetication } from '@/data/usecases/authentication/db-authentication'
+import { JwtAdapter } from '@/infra/criptography/bcrypt-adapter/jwt-adapter/jwt-adapter'
 
 export const makeSignUpController = (): LogControllerDecorator => {
   const salt = 12
+  const jwtAdapter = new JwtAdapter(env.jtwSecret)
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation())
+  const dbAuthetication = new DbAuthetication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
+  const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation(), dbAuthetication)
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signUpController, logMongoRepository)
 }
