eafe73366e5de1f00e5a41ddaa4337616f4bef09
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-06-26 16:04:46 -0300

    feat: add AddSurvey composition

--- src/main/adapters/express/express-routes-adapter.ts
+++ src/main/adapters/express/express-routes-adapter.ts
@@ -6,9 +6,9 @@ export const adaptRoute = (controller: Controller) => {
       body: req.body
     }
     const httpResponse = await controller.handle(httpRequest)
-    if (httpResponse.statusCode !== 200) {
-      return res.status(httpResponse.statusCode).json({ error: httpResponse.body.message })
+    if (httpResponse.statusCode >= 200 || httpResponse.statusCode <= 299) {
+      return res.status(httpResponse.statusCode).json(httpResponse.body)
     }
-    return res.status(httpResponse.statusCode).json(httpResponse.body)
+    return res.status(httpResponse.statusCode).json({ error: httpResponse.body.message })
   }
 }
