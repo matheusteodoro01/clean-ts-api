6a607b1a5e9eb7a2dd382e7560015d0ef01cda31
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-06-26 15:15:30 -0300

    test: ensure SurveyMongoRepository return an survey on add success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,44 @@
+import { AddSurveyModel } from '@/domain/usecases/add-survey'
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { SurveyMongoRepository } from './survey-mongo-repository'
+
+let surveyCollection: Collection
+
+const makeSurveyData = (): AddSurveyModel => ({
+  question: 'any_question',
+  answers: [
+    {
+      answer: 'any_answer',
+      image: 'any_image'
+    },
+    {
+      answer: 'other_answer'
+    }
+  ]
+})
+describe('Survey Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(global.__MONGO_URI__)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('survey')
+    await surveyCollection.deleteMany({})
+  })
+
+  const makeSut = (): SurveyMongoRepository => {
+    return new SurveyMongoRepository()
+  }
+
+  test('Should return an survey on add success', async () => {
+    const sut = makeSut()
+    const survey = await sut.add(makeSurveyData())
+    expect(survey).toBeTruthy()
+    expect(survey?.question).toBe('any_question')
+  })
+})
