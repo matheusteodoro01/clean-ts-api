52109260853d18f4820e76efd5055e3f83ff53ca
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-06-29 22:55:23 -0300

    refactor: organize main layer folders

---
+++ src/main/factories/controllers/usecases/survey/add-survey/db-add-sruvey-factory.ts
@@ -0,0 +1,7 @@
+import { DbAddSuvery } from '../../../../../../data/usecases/add-survey/db-add-survey'
+import { SurveyMongoRepository } from '../../../../../../infra/db/mongodb/survey/survey-mongo-repository'
+
+export const makeDbAddSurvey = (): DbAddSuvery => {
+  const surveyMongoRepository = new SurveyMongoRepository()
+  return new DbAddSuvery(surveyMongoRepository)
+}
--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
 
-import { makeSignUpController } from '../factories/controllers/singup/signup-factory'
-import { makeLoginController } from '../factories/controllers/login/login-factory'
-import { adaptRoute } from '../adapters/express/express-routes-adapter'
+import { makeSignUpController } from '../factories/controllers/login/singup/signup-factory'
+import { makeLoginController } from '../factories/controllers/login/login/login-factory'
+import { adaptRoute } from '../adapters/express-routes-adapter'
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
   router.post('/login', adaptRoute(makeLoginController()))
