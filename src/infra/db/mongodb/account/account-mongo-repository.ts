9910ec7c8ff6ae32d08af09b13b4706e4311bc36
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-06-30 23:56:33 -0300

    fix: ensure: Account Mongo Repository return an account on loadByToken if user admin

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -24,7 +24,7 @@ export class AccountMongoRepository implements AddAcountRepository, LoadAccountB
 
   async loadAccountByToken (token: string, role?: string): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
-    const account = await accountCollection.findOne({ accessToken: token, role })
+    const account = await accountCollection.findOne({ accessToken: token, $or: [{ role }, { role: 'admin' }] })
     return account && MongoHelper.map(account)
   }
 
