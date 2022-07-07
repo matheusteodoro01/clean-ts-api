c048ef1800fa8082288b7a632e4f14b10c73d16c
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-07-06 22:51:27 -0300

    feat: ensure LoadSurveysController calls LoadSurveys

---
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -0,0 +1,10 @@
+
+import { Controller, HttpRequest, HttpResponse, LoadSurveys, ok } from './load-surveys-protocols'
+
+export class LoadSurveysController implements Controller {
+  constructor (private readonly loadSurveys: LoadSurveys) {}
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const surveys = await this.loadSurveys.load()
+    return ok(surveys)
+  }
+}
