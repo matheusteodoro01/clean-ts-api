4e7d618e8be133203e1ece417665fa2969e240bc
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-07-07 23:16:30 -0300

    refactor: rename repository load surveys to a loadAll

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -20,7 +20,7 @@ const makeFakeSurveys = (): SurveyModel[] => (
 
 const makeLoadSurveysRepositoryStub = (): LoadSurveysRepository => {
   class LoadSurveysRepositoryStub implements LoadSurveysRepository {
-    async load (): Promise<SurveyModel[]> {
+    async loadAll (): Promise<SurveyModel[]> {
       return await Promise.resolve(makeFakeSurveys())
     }
   }
@@ -35,14 +35,14 @@ const makeSut = (): SutTypes => {
 describe('DbLoadSurveys ', () => {
   test('Should LoadSurveysRepository ', async () => {
     const { sut, loadSurveysRepositoryStub } = makeSut()
-    const loadSurveysRepositorySpy = jest.spyOn(loadSurveysRepositoryStub, 'load')
+    const loadSurveysRepositorySpy = jest.spyOn(loadSurveysRepositoryStub, 'loadAll')
     await sut.load()
     expect(loadSurveysRepositorySpy).toHaveBeenCalled()
   })
 
   test('Should throw if LoadSurveysRepository trows', async () => {
     const { loadSurveysRepositoryStub, sut } = makeSut()
-    jest.spyOn(loadSurveysRepositoryStub, 'load').mockReturnValueOnce(Promise.reject(new Error()))
+    jest.spyOn(loadSurveysRepositoryStub, 'loadAll').mockReturnValueOnce(Promise.reject(new Error()))
     const promise = sut.load()
     await expect(promise).rejects.toThrow()
   })
