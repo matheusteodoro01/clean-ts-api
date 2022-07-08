349bb06368aa804beb2e5b9ee16cdd9155284355
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-07-07 22:56:50 -0300

    test: ensure LoadSurveysController return 204 if LoadSurveys returns empty

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,12 +1,12 @@
 
-import { badRequest, Controller, HttpRequest, HttpResponse, LoadSurveys, ok } from './load-surveys-protocols'
+import { badRequest, Controller, HttpRequest, HttpResponse, LoadSurveys, noContent, ok } from './load-surveys-protocols'
 
 export class LoadSurveysController implements Controller {
   constructor (private readonly loadSurveys: LoadSurveys) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const surveys = await this.loadSurveys.load()
-      return ok(surveys)
+      return surveys.length ? ok(surveys) : noContent()
     } catch (error) {
       return badRequest(error)
     }
