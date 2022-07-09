cd0794163a2e68574478f9c7318eceaf83071328
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-07-08 22:41:02 -0300

    feat: ensure SurveyMongoRepository return an surveys on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,8 +1,16 @@
+import { LoadSurveysRepository } from '@/data/protocols/db/survey/load-surveys-repository'
 import { AddSurveyRepository } from '@/data/usecases/add-survey/db-add-survey-protocols'
+import { SurveyModel } from '@/domain/models/survey'
 import { AddSurveyModel } from '../../../../domain/usecases/add-survey'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
+  async loadAll (): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const surveys = await surveyCollection.find().toArray()
+    return surveys as unknown as SurveyModel[]
+  }
+
   async add (survey: AddSurveyModel): Promise<AddSurveyModel> {
     const surveyCollection = MongoHelper.getCollection('surveys')
     const surveyData = await (await surveyCollection).insertOne(survey)
