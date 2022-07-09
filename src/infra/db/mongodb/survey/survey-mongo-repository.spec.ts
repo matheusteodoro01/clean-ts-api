79395a5c5a03c9546d12846693a294378cd97a01
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2022-07-08 22:41:22 -0300

    test: ensure SurveyMongoRepository return an surveys on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -28,18 +28,63 @@ describe('Survey Mongo Repository', () => {
   })
 
   beforeEach(async () => {
-    surveyCollection = await MongoHelper.getCollection('survey')
+    surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
   })
 
   const makeSut = (): SurveyMongoRepository => {
     return new SurveyMongoRepository()
   }
+  describe('add(', () => {
+    test('Should return an survey on add success', async () => {
+      const sut = makeSut()
+      const survey = await sut.add(makeSurveyData())
+      expect(survey).toBeTruthy()
+      expect(survey?.question).toBe('any_question')
+    })
+  })
 
-  test('Should return an survey on add success', async () => {
-    const sut = makeSut()
-    const survey = await sut.add(makeSurveyData())
-    expect(survey).toBeTruthy()
-    expect(survey?.question).toBe('any_question')
+  describe('loadAll()', () => {
+    test('Should return an surveys on success', async () => {
+      await surveyCollection.insertMany([
+        {
+          question: 'any_question',
+          answers: [
+            {
+              answer: 'any_answer',
+              image: 'any_image'
+            },
+            {
+              answer: 'other_answer'
+            }
+          ],
+          date: new Date()
+        },
+        {
+          question: 'other_question',
+          answers: [
+            {
+              answer: 'any_answer',
+              image: 'any_image'
+            },
+            {
+              answer: 'other_answer'
+            }
+          ],
+          date: new Date()
+        }
+
+      ])
+      const sut = makeSut()
+      const surveys = await sut.loadAll()
+      expect(surveys.length).toBe(2)
+      expect(surveys[0].question).toBe('any_question')
+      expect(surveys[1].question).toBe('other_question')
+    })
+    test('Should return empty array if survey does not exists', async () => {
+      const sut = makeSut()
+      const answers = await sut.loadAll()
+      expect(answers).toEqual([])
+    })
   })
 })
